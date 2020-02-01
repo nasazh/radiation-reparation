56f5cf15de6dd8d64aed98b7ce4cd98ff1a25242
aidasr <aidasr@wix.com>
2020-02-01 12:35:39 +0200

    Menu scene with button to go to Game scene

---
+++ Assets/Scripts/MenuButtonScript.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtonScript : MonoBehaviour {
+    public void onMouseClick() {
+        SceneManager.LoadScene("Game", LoadSceneMode.Single);
+    }
+}
